7f8541d693692abfa7bc3943dfc41db3d58e7d85
ggapsang <58983068+ggapsang@users.noreply.github.com>
2023-08-15 23:18:46 +0900

    Create print_hello_world.cpp

---
+++ print_hello_world.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+cout << "Hello World" << endl; //cout : console out. endl : end line
+cout << "Sanghyun" << endl;
+cout << "    /|" << endl;
+cout << "   / |" << endl;
+cout << "  /  |" << endl;
+cout << " /   |" << endl;
+cout << "/__ _|" << endl;
+
+```
+return 0;
+
+```
+}
