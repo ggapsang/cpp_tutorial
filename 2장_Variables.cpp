05731232cf0263323229c08d7e34d2592cb06052
ggapsang <58983068+ggapsang@users.noreply.github.com>
2023-08-17 22:30:54 +0900

    Create 2장_Variables.cpp

---
+++ 2장_Variables.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+string character_name = "Tom";
+int character_age;
+character_age = 50;
+cout << "There once was a man named " << character_name << endl;
+cout << "He was " << character_age << " years old" << endl;
+
+character_name = "Mike";
+
+cout << "He liked the name " << character_name << endl;
+cout << "But did not like being " << character_age << endl;
+
+return 0;
