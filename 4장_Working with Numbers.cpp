ef72af556093d2a8e4b5e173d31e1ba38aa8a55e
ggapsang <58983068+ggapsang@users.noreply.github.com>
2023-08-20 22:37:16 +0900

    Create 4장_Working with Numbers.cpp

---
+++ 4장_Working with Numbers.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+int main() 
+{
+	cout << 40 << endl;
+	cout << 7 + 5 << endl;
+	cout << 7 * 5 << endl;
+	cout << 7 / 5 << endl;
+	cout << 7  % 5 << endl; //mod 연산
+	cout << 4 + 5 * 10 << endl;
+	cout << "" << endl;
+
+	int wnum = 5;
+	double dnum = 5.5;
+
+	wnum++;
+	wnum += 80;
+	cout << wnum << endl;
+
+	cout << 5 + 5.5 << endl;
+
+	cout << 10 / 3 << endl;
+	cout << 10.0 / 3 << endl;
+
+	return 0;
+}
