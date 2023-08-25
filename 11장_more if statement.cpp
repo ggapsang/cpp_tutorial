49f95e86b325b06aecd2160860777b6c55a55b0b
ggapsang <58983068+ggapsang@users.noreply.github.com>
2023-08-26 00:49:20 +0900

    Create 11장_more if statement.cpp

---
+++ 11장_more if statement.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+	#include <cmath> // math 함수
+	#include <string>
+
+	using namespace std;
+
+	int getMax(int num1, int num2, int num3)
+	{
+		int result;
+
+		if (num1 >= num2 && num1 >= num3) // >, <, =<, >=, !=, ==
+		{
+			result = num1;
+		}
+		else if (num2 > num1 && num2 > num3)
+		{
+			result = num2;
+		}
+		else
+		{
+			result = num3;
+		}
+
+		return result;
+	}
+
+	int main() 
+	{
+		cout << getMax(20, 3, 20);
+		return 0;
+	}
