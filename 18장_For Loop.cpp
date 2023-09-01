4520113e3a36fe7a7d8399b7d883396c19bb1960
ggapsang <58983068+ggapsang@users.noreply.github.com>
2023-09-02 01:52:16 +0900

    Create 18장_For Loop.cpp

---
+++ 18장_For Loop.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+	#include <cmath> // math 함수
+	#include <string>
+
+	using namespace std;
+
+	int main() 
+	{
+		for (int i = 1; i <= 5; i++)
+		{
+			cout << i << endl;
+		}
+		
+		int nums[] = { 1, 2, 5, 7, 3 };
+		for (int i = 0; i < 5; i++)
+		{
+			cout << nums[i] << endl;
+		}
+		
+		return 0;
+	}
